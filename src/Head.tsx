ca3f5aaadef7e9a526eac657401d24b5ddffc128
Efrain <efrain@DESKTOP-CAJO901.localdomain>
2022-05-11 23:23:30 -0500

    snapshot api

--- src/Head.tsx
+++ src/Head.tsx
@@ -3,10 +3,10 @@ import {Helmet} from 'react-helmet';
 export default function Head() {
   return (
     <Helmet>
-      <title>Tribute</title>
+      <title>Brickdao</title>
       <meta
         name="description"
-        content="A modular DAO framework developed and coordinated by its members"
+        content="brickDAO aims to unlock the current donor ecosystem into a decentralized autonomous organization for donations A transparent charity for the people to the people."
       />
     </Helmet>
   );
